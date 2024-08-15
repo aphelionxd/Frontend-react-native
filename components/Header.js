4703392449cabfa1ee761f4688e3c99078c945c1
thurXNZ <arthur.mello@aluno.ifsp.edu.br>
2024-08-15 15:20:51 -0300

    noco

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,17 @@
 import { View, Text, StyleSheet } from "react-native";
+import { Image } from 'expo-image'
 
 export default function Header(){
     return(
         <View style={styles.header}>
-        <Text>Header OK</Text>
+            <View style={styles.user}>
+              <Image 
+                style={styles.avatar} 
+                source="https://images.jota.info/wp-content/uploads/2015/06/neymar-brazil-wallpaper-confederations-cup-2013.jpg" />
+              <Text style={styles.name}>NJR</Text>
+            </View>
+            
+        <Text>Menu</Text>
       </View>
     )
 }
@@ -11,7 +19,25 @@ export default function Header(){
 const styles = StyleSheet.create({
   header: {
     flex: 1,
+    // backgroundColor: 'black',
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingHorizontal: 15
+  },
+  user: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 10
+  },
+  avatar: {
+    width: 50,
+    height: 50,
+    borderRadius: 25
+  },
+  name: {
+    fontWeight: '600',
+    fontSize: 18
   }
 })
